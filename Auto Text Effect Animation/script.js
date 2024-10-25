1434625b5acb791f73a9a9b781eb404a1249c47a
theflyestnerd <kolaolari@gmail.com>
2024-10-25 19:38:41 +0100

    Auto Text Effect Animation

---
+++ Auto Text Effect Animation/script.js
@@ -0,0 +1,26 @@
+const containerElement = document.querySelector(".container")
+
+const careers = ["Software Architect", "Machine Learning Engineer", "Freelance Web Developer", 
+    "Comic Writer", "Stylish Man", "Athlete"]
+
+let careersIndex = 0
+let charIndex = 0
+
+updateText()
+
+function updateText(){
+    charIndex++
+    containerElement.innerHTML = `
+    <h1>I am ${careers[careersIndex].slice(0,1) === "A" ? "an" : "a"} 
+        ${careers[careersIndex].slice(0,charIndex)}</h1>`
+
+    if(charIndex === careers[careersIndex].length){
+        careersIndex++
+        charIndex = 0
+    }
+    if(careersIndex === careers.length){
+        careersIndex = 0
+    }
+    setTimeout(updateText, 400)
+}
+
