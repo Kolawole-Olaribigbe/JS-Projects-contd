7b199a0ace293b875a2410eb633b7cb4b9d8125a
theflyestnerd <kolaolari@gmail.com>
2024-11-18 19:07:44 +0100

    Jokes Generator Fourth commit

--- Jokes Generator/script.js
+++ Jokes Generator/script.js
@@ -1,1 +1,34 @@
-const 
+const btnComp = document.getElementById("btn")
+const jokeComp = document.getElementById("joke")
+
+const apiKey = "mEGryen0w3F6pIEzjvBmWQ==uY9qMXdaVNi0zzGi"
+const options = {
+    method: "GET",
+    headers: {
+        "X-Api-Key": apiKey
+    }
+}
+const apiURL = "https://api.api-ninjas.com/v1/jokes"
+
+async function getJoke() {
+    try {
+        jokeComp.innerText = "Loading joke..."
+        btnComp.disabled = true
+        btnComp.innerText = "Loading"
+
+        const response = await fetch(apiURL, options)
+        const data = await response.json()
+
+        btnComp.disabled = false
+        btnComp.innerText = "Tell me a joke"
+        
+        jokeComp.innerText = data[0].joke
+        
+    } catch (error) {
+        jokeComp.innerText = "An error occurred"
+        btnComp.disabled = false
+        btnComp.innerText = "Tell me a joke"
+        console.log(error)
+    }
+}
+btnComp.addEventListener("click", getJoke)
