8b8d8640d6b8cf4c1f305886201c3b420b412137
theflyestnerd <kolaolari@gmail.com>
2024-11-21 22:01:11 +0100

    Pomodoro Timer First commit

---
+++ Pomodoro Timer/script.js
@@ -0,0 +1,43 @@
+const startBtn = document.getElementById("start")
+const stopBtn = document.getElementById("stop")
+const resetBtn = document.getElementById("reset")
+const timerComp = document.getElementById("timer")
+
+let interval
+let timeLeft = 1500
+
+function updateTimer() {
+    let minutes = Math.floor(timeLeft / 60)
+    let seconds = timeLeft % 60
+    let formattedTime = `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`
+
+    timerComp.innerHTML = formattedTime
+}
+
+function startTimer() {
+    interval = setInterval(() => {
+        timeLeft--
+        updateTimer()
+
+        if (timeLeft === 0) {
+            clearInterval(interval)
+            alert("Time elapsed")
+            timeLeft = 1500
+            updateTimer()
+        }
+    }, 1000)
+    
+}
+function stopTimer() {
+    clearInterval(interval)
+    
+}
+function resetTimer() {
+    clearInterval(interval)
+    timeLeft = 1500
+    updateTimer()
+    
+}
+startBtn.addEventListener("click", startTimer)
+stopBtn.addEventListener("click", stopTimer)
+resetBtn.addEventListener("click", resetTimer)
