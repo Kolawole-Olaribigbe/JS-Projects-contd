0cda3591b30962e955530b0ba1a5289210678d2e
theflyestnerd <kolaolari@gmail.com>
2024-12-04 20:15:54 +0100

    Sticky Navbar Third commit

---
+++ Sticky Navbar/script.js
@@ -0,0 +1,14 @@
+const navBar = document.querySelector(".navbar")
+const bottomContainer = document.querySelector(".bottom-container")
+
+console.log(bottomContainer.offsetHeight)
+console.log(bottomContainer.offsetTop)
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > bottomContainer.offsetTop - navBar.offsetHeight - 50) {
+        navBar.classList.add("active")
+    } else {
+        navBar.classList.remove("active")
+    }
+    
+})
