d41bc514b27c8b9db4dc965a9586ae2bbf8c3cee
theflyestnerd <kolaolari@gmail.com>
2024-12-09 18:22:45 +0100

    Video Trailer Popup Fourth commit

---
+++ Video Trailer Popup/main.js
@@ -0,0 +1,13 @@
+const btnComp = document.querySelector(".btn")
+const closeIcon = document.querySelector(".close-icon")
+const trailerContainer = document.querySelector(".trailer-container")
+const videoComp = document.querySelector("video")
+
+btnComp.addEventListener("click", () => {
+    trailerContainer.classList.remove("active")
+})
+closeIcon.addEventListener("click", () =>{
+    trailerContainer.classList.add("active")
+    videoComp.pause()
+    videoComp.currentTime = 0
+})
